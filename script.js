1ebe34fd17f73cd1df0852ecc698398563b9b3a8
steuser006 <alleste70@gmail.com>
2026-08-05 17:20:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -491,77 +491,22 @@ if(revealElements.length){
 const faqItems = document.querySelectorAll(".faq-item");
 
 
-if(faqItems.length){
+faqItems.forEach(item => {
 
 
-    faqItems.forEach(item=>{
+    const button = item.querySelector(".faq-question");
 
 
-        const question = item.querySelector(".faq-question");
-
-        const answer = item.querySelector(".faq-answer");
+    button.addEventListener("click", () => {
 
 
-
-        if(question && answer){
-
-
-            question.addEventListener("click",()=>{
-
-
-                const isActive =
-                item.classList.contains("active");
-
-
-
-                faqItems.forEach(other=>{
-
-
-                    other.classList.remove("active");
-
-
-                    const otherAnswer =
-                    other.querySelector(".faq-answer");
-
-
-                    if(otherAnswer){
-
-                        otherAnswer.style.maxHeight = null;
-
-                    }
-
-
-                });
-
-
-
-
-                if(!isActive){
-
-
-                    item.classList.add("active");
-
-
-                    answer.style.maxHeight =
-                    answer.scrollHeight + "px";
-
-
-                }
-
-
-            });
-
-
-        }
+        item.classList.toggle("active");
 
 
     });
 
 
-}
-
-
-
+});
 
 /* ==========================================================
    GALLERY LIGHTBOX
